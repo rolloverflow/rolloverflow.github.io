af2157d57aea7f3e9083ba763580bfc451884e42
rolloverflow <57006611+rolloverflow@users.noreply.github.com>
2019-10-26 16:34:19 +0200

    Revised Mechanics for Mighty Deeds

--- mechanics/js/script.js
+++ mechanics/js/script.js
@@ -20,9 +20,11 @@ function addOption(selectbox,text,value )
 
 function addOption_list(){
 
-    // Define array containing choices for dropdown menus 1 and 2
+    // Define array containing choices for dropdown menus 1, 2, 3 and 4
     var list1 = deeds.byWeapon
     var list2 = deeds.Tables
+    var list3 = new Array("1: One-Man Army", "2: 'It's Fifty-Fifty'", "3: 'I'll Take My Chances...'");
+    var list4 = new Array("Apply Advantage", "Apply Disadvantage")
 
     for (var i=0; i < list1.length;++i){
         entry = list1[i].weapon
@@ -33,6 +35,17 @@ function addOption_list(){
         entry = list2[i].name
        addOption(document.drop_deeds.Deeds_list, entry, entry);
     }
+
+    for (var i=0; i < list3.length;++i){
+      entry = list3[i]          // Note the difference - list3 is an array, not an object
+     addOption(document.drop_modes.Modes_list, entry, entry);
+    }
+
+     for (var i=0; i < list4.length;++i){
+      entry = list4[i]          // Note the difference - list3 is an array, not an object
+     addOption(document.drop_adv.Advan_list, entry, entry);
+    }
+
 }
 
 // Look up Deeds Rolls
@@ -40,12 +53,33 @@ function addOption_list(){
 var weaponDMG, deedDie
 
 function lookupDeed() {
+  
+  // Identify selected mode mechanic 
+  let modePick = document.querySelector("#Modes_list option:checked").index
+
+// Roll the Dice
 
   let deedPick = deeds.Tables[document.getElementById("Deeds_list").selectedIndex-1];
   weaponDMG = document.getElementById("inputWeaponDMG").value;
-  deedDie = document.getElementById("inputDeedDie").value
-  let rollDMG = Math.floor(Math.random() * weaponDMG) + 1;
+  deedDie = document.getElementById("inputDeedDie").value;
   let rollDeedDie = Math.floor(Math.random() * deedDie) + 1;
+
+  switch (document.querySelector("#Advan_list option:checked").index) {
+    case (0):
+      var rollDMG = Math.floor(Math.random() * weaponDMG) + 1;
+      var resultAppen;
+      break;
+
+    case (1): // Roll with Advantage
+    var rollDMG = Math.max(Math.floor(Math.random() * weaponDMG) + 1, Math.floor(Math.random() * weaponDMG) + 1);
+    var resultAppen = " (ADV)";
+    break;
+    
+    case (2): // Roll with Disadvantage
+    var rollDMG = Math.min(Math.floor(Math.random() * weaponDMG) + 1, Math.floor(Math.random() * weaponDMG) + 1);
+    var resultAppen = " (DIS)";
+    break;
+  }
 
   // Check for Mighty Fumbles or Crits 
   if (rollDMG == 1 && rollDeedDie == 1){
@@ -56,28 +90,49 @@ function lookupDeed() {
     rollDMG = rollDMG + Number(document.getElementById("inputWeaponDMGMod").value);
       alert("Nice one - Mighty Critical!");
   } else {
-  // If none, roll on Deed table with modifiers
+
+  // If none, roll on Deed table with modifiers and given selected mode
 
   rollDMG = rollDMG + Number(document.getElementById("inputWeaponDMGMod").value);
   rollDeedDie = rollDeedDie + Number(document.getElementById("inputDeedDieMod").value);
 
-  switch(true){
-    case (3<=rollDeedDie && rollDeedDie <7):
+  switch (true){
 
-      result = deedPick["rolls"][String(rollDeedDie)];
+    // Define failure on "Fifty-Fifty" mode
+    case (modePick == 2 && rollDMG < Math.floor(weaponDMG * 0.5)):
+      result = "Deed Unsuccessful - Attack deals Weapon DMG roll only.";
+      alert("Deed Unsuccessful!");
       break;
-      case (7<=rollDeedDie):
-        result = deedPick["rolls"]["7"];
+    
+   // Define failure on "I take my chances" mode   
+     case (modePick == 3 && rollDMG < Math.floor(weaponDMG * 0.85)):
+        result = "Deed Unsuccessful - Attack deals Weapon DMG roll only.";
+        alert("Deed Unsuccessful!");
         break;
-        default:
-          result = "Deed Unsuccessful - Attack deals Weapon DMG roll only.";
-          alert("Deed Unsuccessful!");
-    }
-  }
+    
+    default:
+      switch(true){
+      case (3<=rollDeedDie && rollDeedDie <7):
+        result = deedPick["rolls"][String(rollDeedDie)];
+        break;
+        
+        case (7<=rollDeedDie):
+          result = deedPick["rolls"]["7"];
+          break;
+          
+          default:
+            result = "Deed Unsuccessful - Attack deals Weapon DMG roll only.";
+            alert("Deed Unsuccessful!");
+          }
+        }
+      }
 
+
+
+// Output results
   document.getElementById("description").innerHTML = deedPick["description"]
 
-  addTableRow(result, rollDMG, rollDeedDie)
+  addTableRow(result, rollDMG + resultAppen, rollDeedDie)
 }
 
     
